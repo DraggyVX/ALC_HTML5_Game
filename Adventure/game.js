4fbf1b16fff8a7bc2ad45a68e0e74c4ff73b5434
DraggyVX <217Dragonmaster@gmail.com>
2019-01-22 10:29:03 -0700

    Made files

---
+++ Adventure/game.js
@@ -0,0 +1,12 @@
+/*
+alert("Warning! Will Robinson...Warning!!");
+
+confirm("Do you like pokemon?"); //Two options
+
+prompt("What type of pokemon?"); //A written response
+*/
+
+var playerName = prompt("What is your name traveller?");
+
+document.write(playerName);
+confirm(playerName + " what way do you want to go?");
