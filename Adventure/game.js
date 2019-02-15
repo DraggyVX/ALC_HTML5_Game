804dc8e595bd6934a3f2afc689e8690226c223ad
DraggyVX <217Dragonmaster@gmail.com>
2019-02-15 10:30:44 -0700

    added restart function

--- Adventure/game.js
+++ Adventure/game.js
@@ -1,9 +1,14 @@
 var input;
-var room = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30, 50, 100, 150, 200, 666];
-var weapon = ["fist", "basic-sword", "club"];
-var items = ["lanturn"];
+var room = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 100, 150, 200, 666];
+var fist = false; //will make a function will all weapons and items later 
+var basicSword = false;
+var club = false;
+var swordOfTree = false;
+var items = ["lantern"];
 var deathCounter = 0;
-var acheievments = ["died-more-than-10", "died-more-than-100"];
+var acheievments = ["died-more-than-10", "died-more-than-100", "Friend-of-trees"];
+var element = document.getElementById("unorderedList");
+var treeAlive = true;
 
 Game();
 
@@ -17,7 +22,6 @@ Game();
         var newListElement = document.createElement("li"); 
         var node = document.createTextNode(input); 
         newListElement.appendChild(node); 
-        var element = document.getElementById("unorderedList");
         element.appendChild(newListElement);
         //Resets user-input to blank
         document.getElementById("user-input").value = ""; 
@@ -25,8 +29,8 @@ Game();
         preformAction();
         Achievement();
         newListElement.scrollIntoView(); 
-        }
     }
+}
 
 //Starts the game
 function Game() {
@@ -38,13 +42,12 @@ function Game() {
     Output("-go east (to the town)");
     Output("-go south (to the plains)");
     Output("-go west (to the forest)");
-    Output("-go back to sleep");
+    Output("-go back to sleep"); 
     room = 0;
 }
 
 //Preform Action Thank you Jaiden
-function preformAction() 
-{
+function preformAction() {
     switch(room) 
     {
         case 0: //under the tree (Starting place)
@@ -105,9 +108,12 @@ function preformAction()
                         break;
                             
                         case "investigate tree":
-                        Output("You start moving around the tree looking at it.");
-                        Output("Hello sir, why are you looking at me like that?");
-                        room = 200;
+                        if (treeAlive = true) {
+                            Output("You start moving around the tree looking at it.");
+                            Output("What do you want? Why are you looking at me like that?");
+                            room = 200;
+                        }
+                        Output("Sorry thats not gonna work again ;)");
                         break;
                     }
             break;
@@ -128,7 +134,7 @@ function preformAction()
                         
                     case "go into the swamp": //death
                     case "go to swamp":
-                    case "go into the swamp":
+                    case "go into swamp":
                         Output("You get stuck in mud giant leeches come out from the water and eat you alive. *You have died*");
                         Output("-restart?");
                         room = 666;
@@ -196,19 +202,30 @@ function preformAction()
                 case "break the door down":
                 case "destroy door":
                     Output("");
+                    Output("");
+                    Output("");
+                    
                 break;
                     
                 case "try to open door":
                     Output("You try to open the door but it doesn't budge.");
+                    Output("");
+                    Output("");
                     room = 3;
                 break;
                 
                 case "go through the window":
                     Output("You break through the window. You look around this old house and see you’re in a small room with a cauldron in the middle with shelves along the walls. The shelves have a bunch of stuff ranging from books to potions.");
+                    Output("");
+                    Output("");
+                    Output("");
                 break;    
                     
                 case "look around old looking house":
                     Output("You look around the old looking house. The old looking house looks old and abandoned. There is a front door and a window next to it. You notice there is a hatch on the side of the old looking house.");
+                    Output("");
+                    Output("");
+                    Output("");
                 break;
             }
         break;
@@ -273,15 +290,28 @@ function preformAction()
             switch (input.toLowerCase()) 
                 {
                     case "go around the wall":
-                        Output("");
+                        Output("you walk around the wall to the other side and see another big gate.");
+                        Output("-try to go through the gate");
+                        Output("-keep going around");
+                        Output("-go back");
+                        room = 51;
                     break;
                     
                     case "try going through the gate":
-                        Output("");
+                        Output("Halt! Let me see your papers!");
+                        Output("A soldier in his early twenties approaches you.");
+                        if (papers = true) {
+                            Output("-Show him your papers");
+                        }
+                        Output("-run away");
+                        Output("-Try to seduce him");
+                        Output("-Convince him to let you through");
+                        room = 52;
                     break;
                         
                     case "sneak into a carriage":
-                        Output("");
+                        Output("You see a merchants carriage and jump inside. You look around it is dark and there are several crates.");
+                        room = 53;
                     break;
                         
                     case "go back to your tree":
@@ -308,11 +338,20 @@ function preformAction()
         case 200:
             switch (input.toLowerCase())
                 {
-                    case "sorry":
-                        Output("It's okay I am the only tree that can talk in this region. My fellow breathren have fallen to the darn humans who chopped us down and used us for there houses.");
+                    case "looking at your beauty":
+                    case "admiring your trunk":
+                        Output("I am the only tree that can talk in this region. My fellow breathren have fallen to the darn humans who chopped us down and used us for there houses.");
                         Output("I am very old now and am near the end of the line. I would like you to have this.");
                         Output("A sword seems to appear from thin air *Sword of the fallen king*");
-                        room 0;
+                        swordOfTree = true;
+                        treeAlive = false;
+                        room = 0;
+                    break;
+                    default:
+                        Output("You greedy human!! DIE! *You have died*");
+                        Output("-restart?");
+                        Output("-quit");
+                        room = 666;
                     break;
                 }
             
@@ -322,7 +361,7 @@ function preformAction()
                 {
                     case "restart":
                     case "start over":
-                        Output("Darn you lost... restarting.... Good luck!");
+                        restart();
                         Game();
                     break;
                         
@@ -333,6 +372,12 @@ function preformAction()
         break;
     }
             
+}
+
+function restart() {
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
 }
 
 //Gets the userinput
@@ -362,5 +407,13 @@ function Achievement() {
         var element = document.getElementById("achievements");
         element.appendChild(newListElement);
         acheievments = "died-more-than-10";
+    }
+    else if (swordOfTree == true) {
+        var newListElement = document.createElement("li");
+        var node = document.createTextNode("Friend of Trees");
+        newListElement.appendChild(node);
+        var element = document.getElementById("achievements");
+        element.appendChild(newListElement);
+        swordOfTree = false;
     }
 }
